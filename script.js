2ed1d938ee4120e4984117dd7c3008bb7bdc8526
Kushaal Kankane <kushaalkankane12345@gmail.com>
2025-06-10 16:14:25 +0000

    added different modes and improved ui

--- script.js
+++ script.js
@@ -1,8 +1,61 @@
 const main = document.querySelector(".container");
-
 const gridSize = document.querySelector("#gridSize");
+const colorSelector = document.querySelector("#colorSelector");
+const normal = document.querySelector(".normalMode");
+const grayscale = document.querySelector(".grayScaleMode");
+const rainbow = document.querySelector(".rainbowMode");
+const sizeValue = document.querySelector(".size");
 
 let isMouseDown = false;
+
+let colorMode = "normalMode";
+let baseColor = "black";
+let darkness = 0;
+
+function chooseColor()
+{
+    colorSelector.addEventListener("change", () => {
+        baseColor = colorSelector.value;
+    });
+}
+
+function chooseMode()
+{
+    normal.addEventListener("click", (e) => {
+        colorMode = e.target.className;
+    });
+
+    grayscale.addEventListener("click", (e) => {
+        colorMode = e.target.className;
+    });
+
+    rainbow.addEventListener("click", (e) => {
+        colorMode = e.target.className;
+    });
+}
+
+function startColor(grid)
+{
+    if(colorMode == "normalMode")
+        grid.style.backgroundColor = `${baseColor}`;
+    else if(colorMode == "rainbowMode")
+    {
+        let rand1 = Math.floor(Math.random() * 256);
+        let rand2 = Math.floor(Math.random() * 256);
+        let rand3 = Math.floor(Math.random() * 256);
+        grid.style.backgroundColor = `rgb(${rand1},${rand2},${rand3})`;
+    }
+    else if(colorMode == "grayScaleMode")
+    {
+        let current = parseFloat(grid.dataset.darkness);
+        if(current<1)
+        {
+            current = Math.min(1, current + 0.1); // Cap at 1
+            grid.dataset.darkness = current.toFixed(1);
+            grid.style.backgroundColor = `rgba(0,0,0,${current})`;
+        }
+    }
+}
 
 function enableDrawing()
 {
@@ -12,14 +65,16 @@ function enableDrawing()
     const grids = document.querySelectorAll(".grids");
 
     grids.forEach( grid => {
+        grid.dataset.darkness = "0";
+
         grid.addEventListener("mousedown", (e) => {
-            grid.style.backgroundColor = "black";
+            startColor(e.target);
         });
 
-        grid.addEventListener("mouseover", () => {
+        grid.addEventListener("mouseover", (e) => {
             if(isMouseDown)
             {
-                grid.style.backgroundColor = "black";
+                startColor(e.target);
             }
         });
     });
@@ -41,12 +96,15 @@ function createGrid(size){
 
     enableDrawing();
     clearCanvas();
+    chooseColor();
+    chooseMode();    
 }
 
 createGrid(gridSize.value);
 
 gridSize.addEventListener("change", () => {
     createGrid(gridSize.value);
+    sizeValue.textContent = `${gridSize.value} X ${gridSize.value}`;
 });
 
 
